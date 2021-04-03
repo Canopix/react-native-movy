e0209e4b930beb668ae3b12c023d7446afd1dc45
Leonardo Val <leonardo.val@mooveit-it.com>
2021-04-03 17:06:07 -0300

    Fix TMDB controller's issue (found with Jest)

--- src/controllers/TMDBController.js
+++ src/controllers/TMDBController.js
@@ -1,7 +1,7 @@
 import { Config } from 'react-native-config';
 import { HttpClient } from '@/controllers/HttpClient';
 
-const LANGUAGE_RE = /^[a-z]{2}-[a-z]{2}$/;
+const LANGUAGE_RE = /^[a-zA-Z]{2}-[a-zA-Z]{2}$/;
 
 const checkParamInteger = (name, value) => {
   if (typeof value !== 'number' || Number.isNaN(value)) {
@@ -17,50 +17,57 @@ const checkParamRegExp = (name, value, regexp) => {
 
 // See <https://developers.themoviedb.org/3>.
 export class TMDBController {
-  static async getNowPlaying({ page = 1, language = 'en-US' }) {
+  static async getNowPlaying(args) {
+    const { page = 1, language = 'en-US' } = args || {};
     checkParamInteger('page', page);
     checkParamRegExp('language', language, LANGUAGE_RE);
-    const { data } = await HttpClient.get('/movie/now_playing', {
+    const params = {
       api_key: Config.TMDB_API_KEY,
       page,
       language,
-    });
-
-    return data;
+    };
+    const response = await HttpClient.get('/movie/now_playing', { params });
+    return response;
   }
 
-  static async getPopular({ page = 1, language = 'en-US' }) {
+  static async getPopular(args) {
+    const { page = 1, language = 'en-US' } = args || {};
     checkParamInteger('page', page);
     checkParamRegExp('language', language, LANGUAGE_RE);
-    const { data } = await HttpClient.get('/movie/popular', {
+    const params = {
       api_key: Config.TMDB_API_KEY,
       page,
       language,
-    });
-    return data;
+    };
+    const response = await HttpClient.get('/movie/popular', { params });
+    return response;
   }
 
-  static async getMovieDetails({ movie_id, language = 'en-US' }) {
+  static async getMovieDetails(args) {
+    const { movie_id, language = 'en-US' } = args || {};
     checkParamInteger('movie id', movie_id);
     checkParamRegExp('language', language, LANGUAGE_RE);
     const path = `/movie/${Math.floor(movie_id)}`;
-    const { data } = await HttpClient.get(path, {
+    const params = {
       api_key: Config.TMDB_API_KEY,
       language,
-    });
-    return data;
+    };
+    const response = await HttpClient.get(path, { params });
+    return response;
   }
 
-  static async searchMovies({ query, page = 1, language = 'en-US' }) {
+  static async searchMovies(args) {
+    const { query, page = 1, language = 'en-US' } = args || {};
     checkParamRegExp('query', query, /^.+$/);
     checkParamInteger('page', page);
     checkParamRegExp('language', language, LANGUAGE_RE);
-    const { data } = await HttpClient.get('/search/movie', {
+    const params = {
       api_key: Config.TMDB_API_KEY,
       query,
       page,
       language,
-    });
-    return data;
+    };
+    const response = await HttpClient.get('/search/movie', { params });
+    return response;
   }
 } // class TMDBController
