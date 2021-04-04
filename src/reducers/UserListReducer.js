33f4dc3d486902a3396b5fd026e7c86daec210ab
Leonardo Val <leonardo.val@mooveit-it.com>
2021-04-03 23:46:49 -0300

    Add reducer for the user list's actions

---
+++ src/reducers/UserListReducer.js
@@ -0,0 +1,21 @@
+import { TYPES } from '@/actions/MoviesActions';
+import { movieData } from '@/reducers/utils';
+
+const INITIAL_STATE = {};
+
+const removeById = (movies, idToRemove) => {
+  return idToRemove ? movies.filter(({ id }) => id !== idToRemove) : movies;
+};
+
+export const userListReducer = (state = INITIAL_STATE, { payload, type }) => {
+  switch (type) {
+    case TYPES.USER_LIST_ADD:
+      const newState = removeById(payload?.id);
+      newState.push(movieData(payload));
+      return newState;
+    case TYPES.USER_LIST_REMOVE:
+      return removeById(payload?.id);
+    default:
+      return state;
+  }
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,9 +2,13 @@ import { combineReducers } from 'redux';
 import { errorReducer } from '@/reducers/ErrorReducer';
 import { statusReducer } from '@/reducers/StatusReducer';
 import { userReducer } from '@/reducers/UserReducer';
+import { tmdbConfigurationReducer } from '@/reducers/TMDBConfigurationReducer';
+import { userListReducer } from '@/reducers/UserListReducer';
 
 export const rootReducer = combineReducers({
   error: errorReducer,
   status: statusReducer,
   user: userReducer,
+  tmdbConfiguration: tmdbConfigurationReducer,
+  userList: userListReducer,
 });
